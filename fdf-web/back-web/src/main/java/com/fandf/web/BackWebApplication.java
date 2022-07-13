959259d2037a1dd40466cdc1a0fc7d776375e2d3
fdf <365283959@qq.com>
2022-07-13 10:19:58 +0800

    前端管理平台

---
+++ fdf-web/back-web/src/main/java/com/fandf/web/BackWebApplication.java
@@ -0,0 +1,15 @@
+package com.fandf.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fandongfeng
+ * @date 2022/7/11 18:47
+ */
+@SpringBootApplication
+public class BackWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackWebApplication.class, args);
+    }
+}
