5787585cb9f9ba328f70ed2da050dabd49c9be98
范东风 <365283959@qq.com>
2022-12-29 16:06:23 +0800

    sso单点登录

---
+++ fdf-demo/sso-demo/src/main/java/com/fdf/sso/SsoApplication.java
@@ -0,0 +1,17 @@
+package com.fdf.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fandongfeng
+ * @date 2022-12-29 14:02
+ */
+@SpringBootApplication
+public class SsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
+
+}
