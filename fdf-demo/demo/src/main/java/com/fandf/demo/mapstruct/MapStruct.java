21024c0604fc27973ae2f2c38733bf0858d57bc0
范东风 <365283959@qq.com>
2023-01-06 17:30:11 +0800

    ip2region集成

--- fdf-demo/demo/src/main/java/com/fandf/demo/mapstruct/MapStruct.java
+++ fdf-demo/demo/src/main/java/com/fandf/demo/mapstruct/MapStruct.java
@@ -13,6 +13,37 @@ import org.mapstruct.factory.Mappers;
 @Mapper // 直接使用
 //@Mapper(componentModel = "spring") // 整合 Spring，设置 componentModel = "spring"，需要使用的地方直接通过 @Resource 注入即可
 public interface MapStruct {
+//    与lombok一同使用时 pom 需增加下面插件
+//    <build>
+//        <plugins>
+//            <plugin>
+//                <groupId>org.apache.maven.plugins</groupId>
+//                <artifactId>maven-compiler-plugin</artifactId>
+//                <version>3.6.0</version>
+//                <configuration>
+//                    <source>1.8</source>
+//                    <target>1.8</target>
+//                    <annotationProcessorPaths>
+//                        <path>
+//                            <groupId>org.mapstruct</groupId>
+//                            <artifactId>mapstruct-processor</artifactId>
+//                            <version>1.5.3.Final</version>
+//                        </path>
+//                        <path>
+//                            <groupId>org.projectlombok</groupId>
+//                            <artifactId>lombok</artifactId>
+//                            <version>1.18.24</version>
+//                        </path>
+//                        <path>
+//                            <groupId>org.projectlombok</groupId>
+//                            <artifactId>lombok-mapstruct-binding</artifactId>
+//                            <version>0.2.0</version>
+//                        </path>
+//                    </annotationProcessorPaths>
+//                </configuration>
+//            </plugin>
+//        </plugins>
+//    </build>
 
     MapStruct INSTANCE = Mappers.getMapper(MapStruct.class);
 
