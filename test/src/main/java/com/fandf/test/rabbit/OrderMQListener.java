f211b62c43e3e990054123871d6354a900054f74
fdf <365283959@qq.com>
2023-04-16 07:15:47 +0800

    rabbitmq高级

--- test/src/main/java/com/fandf/test/rabbit/OrderMQListener.java
+++ test/src/main/java/com/fandf/test/rabbit/OrderMQListener.java
@@ -1,9 +1,12 @@
 package com.fandf.test.rabbit;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author fandongfeng
@@ -13,18 +16,31 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = "ikun_queue")
 public class OrderMQListener {
 
-    /**
-     * RabbitHandler 会自动匹配 消息类型（消息自动确认）
-     * @param msg 内存
-     * @param message mq消息
-     */
+//    /**
+//     * RabbitHandler 会自动匹配 消息类型（消息自动确认）
+//     * @param msg 内容
+//     * @param message mq消息
+//     */
+//    @RabbitHandler
+//    public void consumer(String msg, Message message) {
+//        long msgTag = message.getMessageProperties().getDeliveryTag();
+//        System.out.println("msg="+msg);
+//        System.out.println("msgTag="+msgTag);
+//        System.out.println("message="+ message);
+//        System.out.println("监听到消息：消息内容:"+message.getBody());
+//    }
+
     @RabbitHandler
-    public void releaseCouponRecord(String msg, Message message) {
+    public void consumer(String body, Message message, Channel channel) throws IOException {
         long msgTag = message.getMessageProperties().getDeliveryTag();
-        System.out.println("msg="+msg);
         System.out.println("msgTag="+msgTag);
         System.out.println("message="+ message);
-        System.out.println("监听到消息：消息内容:"+message.getBody());
+        System.out.println("body="+body);
+
+        //成功确认，使用此回执方法后，消息会被 rabbitmq broker 删除
+        channel.basicAck(msgTag,false);
+//        channel.basicNack(msgTag,false,true);
+
     }
 
 }
--- test/src/test/java/com/fandf/test/redis/ProductMQTest.java
+++ test/src/test/java/com/fandf/test/redis/ProductMQTest.java
@@ -2,6 +2,7 @@ package com.fandf.test.redis;
 
 import com.fandf.test.rabbit.RabbitMQConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,6 +22,41 @@ public class ProductMQTest {
     public void test() throws InterruptedException {
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME,"ikun.mei", "鸡你太美");
         Thread.sleep(10000);
+    }
+
+    @Test
+    public void testConfirmCallback() throws InterruptedException {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            /**
+             *
+             * @param correlationData 配置
+             * @param ack 交换机是否收到消息，true是成功，false是失败
+             * @param cause 失败的原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm=====>");
+                System.out.println("confirm==== ack="+ack);
+                System.out.println("confirm==== cause="+cause);
+                //根据ACK状态做对应的消息更新操作 TODO
+            }
+        });
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME,"ikun.mei", "鸡你太美");
+        Thread.sleep(10000);
+    }
+
+
+    @Test
+    void testReturnCallback() {
+        //为true,则交换机处理消息到路由失败，则会返回给生产者  配置文件指定，则这里不需指定
+        rabbitTemplate.setMandatory(true);
+        //开启强制消息投递（mandatory为设置为true），但消息未被路由至任何一个queue，则回退一条消息
+        rabbitTemplate.setReturnsCallback(returned -> {
+            int code = returned.getReplyCode();
+            System.out.println("code="+code);
+            System.out.println("returned="+ returned);
+        });
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME,"123456","测试returnCallback");
     }
 
 }
